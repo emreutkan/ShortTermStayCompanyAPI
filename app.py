5132d35c44f6cdbcdd5505e5eddb0fd55d39ec00
emreutkan <127414322+emreutkan@users.noreply.github.com>
2024-11-16 13:38:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -113,5 +113,6 @@ def register():
 
     return jsonify({'message': 'User registered successfully'}), 201
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
